127f0a9f5f3537999cca25d2864b19390d14a8eb
TheNMD <manhdanog@gmail.com>
2023-04-07 08:45:31 +0700

    com

--- src/main/mt22/checker/StaticChecker.py
+++ src/main/mt22/checker/StaticChecker.py
@@ -1,22 +1,64 @@
+from AST import *
 from Visitor import Visitor
 from StaticError import *
-
 class StaticChecker(Visitor):
     
     def __init__(self, ast):
         self.ast = ast
     
     def check(self):
-        return self.visitProgram(self.ast, [])
+        return self.visit(ast=self.ast, param=[])
     
     def visitProgram(self, ast, param):
-        obj = []
-        # print(ast.decls)
-        # raise Redeclared(kind=Variable(),identifier=name)
+        decls = ast.decls
+        for decl in decls:
+            param += [self.visit(decl, param)]
         return ""
             
-    # def visitVarDecl(self, ast, param):
-    #     name = ast.name
-    #     if name in param:
-    #         raise Redeclared(kind=Variable(),identifier=name)
-    #     return name
+    def visitVarDecl(self, ast, param):
+        name, typ, init = ast.name, ast.typ, ast.init
+        if name in param:
+            raise Redeclared(kind=Variable(), identifier=name)
+        # TODO Undeclared
+        if init:
+            # TODO init la BiExpr, UnExpr, ArrayCell, FuncCall, ID
+            if type(typ) is IntegerType:
+                if type(init) is not IntegerLit:
+                    raise TypeMismatchInExpression(init)
+            elif type(typ) is FloatType:
+                if type(init) is not IntegerLit and type(init) is not FloatLit:
+                    raise TypeMismatchInExpression(init)
+            elif type(typ) is BooleanType:
+                if type(init) is not BooleanLit:
+                    raise TypeMismatchInExpression(init)
+            elif type(typ) is StringType:
+                if type(init) is not StringLit:
+                    raise TypeMismatchInExpression(init)
+            elif type(typ) is ArrayType:
+                pass
+            elif type(typ) is AutoType:
+                pass
+            # TODO Xem lai VoidType o ASTGeneration
+        else:
+            if type(typ) is AutoType:
+                raise Invalid(kind=Variable(), name=name)
+        return ast.name
+    
+    def visitIntegerType(self, ast, param): pass
+    def visitFloatType(self, ast, param): pass
+    def visitBooleanType(self, ast, param): pass
+    def visitStringType(self, ast, param): pass
+    def visitArrayType(self, ast, param): pass
+    def visitAutoType(self, ast, param): pass
+    def visitVoidType(self, ast, param): pass
+    
+    def visitBinExpr(self, ast, param): pass
+    def visitUnExpr(self, ast, param): pass
+    def visitId(self, ast, param): pass
+    def visitArrayCell(self, ast, param): pass
+    def visitIntegerLit(self, ast, param): pass
+    def visitFloatLit(self, ast, param): pass
+    def visitStringLit(self, ast, param): pass
+    def visitBooleanLit(self, ast, param): pass
+    def visitArrayLit(self, ast, param): pass
+    def visitFuncCall(self, ast, param): pass
--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -4,6 +4,16 @@ from AST import *
 
 class CheckerSuite(unittest.TestCase):
     def test1(self):
-        input = """x : integer ;"""
-        expect = "None"
+        input = """x : integer ; x : float ;"""
+        expect = "Redeclared Variable: x"
         self.assertTrue(TestChecker.test(input, expect, 401))
+        
+    def test2(self):
+        input = """x : integer = 2.0 ;"""
+        expect = "Type mismatch in expression: FloatLit(2.0)"
+        self.assertTrue(TestChecker.test(input, expect, 402))
+
+    def test3(self):
+        input = """x : auto ;"""
+        expect = "Invalid Variable: x"
+        self.assertTrue(TestChecker.test(input, expect, 403))
